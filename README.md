00f646c26325a307c8f64e543c2b03ff13bd2a10
Matheus Medeiros Oselame <matheusmedeiros2003@gmail.com>
2022-12-20 17:58:57 -0300

    criada verificação para saber se a tabela ja existe no banco

---
+++ README.md
@@ -0,0 +1,20 @@
+# BaseProjectPHP
+
+Projeto desenvolvido para criar uma base de projeto em php. 
+
+## Instalação das dependencias do projeto
+``` bash
+    composer install
+```
+
+### FrameWork Doctrine ORM
+
+O framework Doctrine ORM, é utilizado para transformar as entidades criadas dentro da pasta "\Entity".
+Todas as entidades criadas virarão as colunas do banco de dados, juntamente com as suas propriedades
+definidas quando o projeto for iniciado. 
+
+Para que uma entidade possa ser mapeada pelo Doctrine e criada no banco, é necessário  usar as Annotations (verificar na documentação). Pelas annotations é definido o tipo, nome e demais atributos das colunas ou campos no banco.
+
+
+
+##
--- src/Controller/StartProjectController.php
+++ src/Controller/StartProjectController.php
@@ -7,11 +7,13 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Nbwdigital\BaseProject\Entity\User;
 use Doctrine\ORM\Tools\SchemaTool;
-use Exception;
 use Doctrine\ORM\Tools\SchemaValidator;
 use Nbwdigital\BaseProject\Infra\EntityManagerFactory;
 use Nbwdigital\BaseProject\Repository\CursoRepository;
 use Nbwdigital\BaseProject\Traits\Utils;
+
+// use Doctrine\ORM\Exception\ORMException;
+use Doctrine\DBAL\Exception;
 
 class StartProjectController
 {
@@ -22,42 +24,38 @@ class StartProjectController
     {
         try {
 
-            $curso = new CursoRepository();
-            $cursos = $curso->getAll();
-            $response->getBody()->write(json_encode($cursos));
-            return $response;
-
-        } catch (Exception $e) {
-
-            try {
-                $class_name = get_class(new User());
-
-                $reflection_class = new \ReflectionClass($class_name);
-
-                $entityManagerFactory = new EntityManagerFactory();
-                $entityManager = $entityManagerFactory->getEntityManager();
+            // $curso = new CursoRepository();
+            // $cursos = $curso->getAll();
+            // $response->getBody()->write(json_encode($cursos));
+            // return $response;
 
 
+            $entityManagerFactory = new EntityManagerFactory();
+            $entityManager = $entityManagerFactory->getEntityManager();
+
+            $connection = $entityManagerFactory->GetConnectionDb();
+
+            $schemaManager = $connection->createSchemaManager();
+
+            if ($schemaManager->tablesExist(['User', 'Invoice'])) {
+                $response->getBody()->write("tabelas ja existem no banco");
+                return $response;
+            } else {
                 $tool = new SchemaTool($entityManager);
                 $classes = [
-                    $entityManager->getClassMetadata($reflection_class->name),
+                    $classMetadata = $entityManager->getClassMetadata("Nbwdigital\BaseProject\Entity\User"),
+                    $classMetadata = $entityManager->getClassMetadata("Nbwdigital\BaseProject\Entity\Invoice")
                 ];
-
-                // $validator = new SchemaValidator($entityManager);
-                // $errors = $validator->validateMapping();
-
-                // if (count($errors) > 0) {
-                //     // Lots of errors!
-                //     echo implode("\n\n", $errors);
-                // }
 
                 $tool->createSchema($classes);
                 $response->getBody()->write("tabela criada no banco de dados");
                 return $response;
-            } catch (Exception $e2) {
-                $this->writeToLog($e->getMessage(), "URL - ERROR");
-                return $response->getBody()->write("erro fudido");
             }
+        } catch (Exception $e) {
+
+
+            $this->writeToLog($e->getMessage(), "URL - ERROR");
+            return $response->getBody()->write("erro fudido");
         }
     }
 }
